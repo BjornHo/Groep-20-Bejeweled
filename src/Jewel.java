99cdb90c1000df95137e48b7b655169d2172d5fd
B.Ho <bjornho@outlook.com>
2015-09-03 17:06:15 +0200

    Resizing and Jewel Class
    I adjusted the image sizes to match each other. I started making the Jewel
    constructor. Added several methods including isAdjacent and isSameColour.

---
+++ src/Jewel.java
@@ -0,0 +1,35 @@
+
+public class Jewel {
+	
+	protected enum Colour { Blue, Green, Orange, Purple, Red, White, Yellow}; 
+	protected Colour colour;
+	protected int xCoord;
+	protected int yCoord;
+	
+	public Jewel(Colour colour, int xCoord, int yCoord) {
+		this.colour = colour;
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
+	}
+	
+	public boolean isAdjacent(Jewel that) {
+		if(this.xCoord == that.xCoord && Math.abs(this.yCoord - that.yCoord) == 1){
+			return true;
+		}
+		else if(this.yCoord == that.yCoord && Math.abs(this.xCoord - that.xCoord) == 1){
+			return true;
+		}
+		else
+			return false;				
+	}
+	
+	public boolean isSameColour(Jewel that) {
+		if(this.colour == that.colour) {
+			return true;
+		}
+		
+		else
+			return false;
+	}
+	
+}
