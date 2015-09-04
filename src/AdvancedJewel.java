96b4e055db138d93d9022f671f0d7d40b45cc70d
Hugo Kooijman <H.V.Kooijman@gmail.com>
2015-09-04 18:02:15 +0200

    Optional Jewel class that stores coordinates as well

---
+++ src/AdvancedJewel.java
@@ -0,0 +1,23 @@
+/**
+ * @author hvkooijman
+ * Jewel class that does store coordinates within it's Jewel Objects.
+ */
+public class AdvancedJewel extends Jewel {
+
+	private Coordinate coordinate;
+	
+	/**
+	 * AdvancedJewel constructor method.
+	 * 
+	 * @param colour 
+	 * 			Colour of the Jewel.
+	 * @param c
+	 * 			Coordinate of the Jewel.
+	 */
+	public AdvancedJewel(Colour colour, Coordinate c) {
+		super(colour);
+		this.coordinate.setX(c.getX());
+		this.coordinate.setY(c.getY());
+	}
+
+}
