828cea0cbd6a7a26f2662ec305be33ba9cdcac4c
Hugo Kooijman <H.V.Kooijman@gmail.com>
2015-09-04 19:19:20 +0200

    Coordinate class javadoc

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -22,7 +22,8 @@ public class Coordinate {
 	/**
 	 * Getter method for returning the x-value of a Coordinate.
 	 * 
-	 * @return x-value of a Coordinate.
+	 * @return int
+	 * 				x-value of a Coordinate.
 	 */
 	public int getX() {
 		return x;
@@ -38,10 +39,14 @@ public class Coordinate {
 	}
 	
 	/**
-	 * Returns true iff c1 and c2 are either horizontally or vertically adjacent.
-	 * @param c1 Coordinate 1
-	 * @param c2 Coordinate 2
-	 * @return True if c1 and c2 are horizontally or vertically adjacent, false otherwise.
+	 * Method for checking if 2 Jewel coordinates are either horizontally or vertically adjacent.
+	 * 
+	 * @param Coordinate c1
+	 * 				Coordinate 1.
+	 * @param Coordinate c2
+	 * 				Coordinate 2.
+	 * @return boolean
+	 * 				True if c1 and c2 are either horizontally or vertically adjacent, false otherwise.
 	 */
 	public static boolean areAdjacent(Coordinate c1, Coordinate c2) {
 		return ( (Math.abs(c1.x - c2.x) == 1 && c1.y == c2.y) //Horizontal adjacency check
@@ -52,8 +57,10 @@ public class Coordinate {
 	/**
 	 * Getter method for acquiring a Jewel located north of a designated Jewel.
 	 * 
-	 * @param c Coordinate of a Jewel of which the northern jewel is requested.
-	 * @return A Coordinate located north of Coordinate c.
+	 * @param Coordinate c
+	 * 				Coordinate of a Jewel of which the northern jewel is requested.
+	 * @return Coordinate
+	 * 				A Coordinate located north of Coordinate c.
 	 */
 	public Coordinate getNorth() {
 		Coordinate NorthJewel = new Coordinate(this.x, (this.y + 1));
@@ -63,8 +70,10 @@ public class Coordinate {
 	/**
 	 * Getter method for acquiring a Jewel located west of a designated Jewel.
 	 * 
-	 * @param c Coordinate of a Jewel of which the western jewel is requested.
-	 * @return A Coordinate located west of Coordinate c.
+	 * @param Coordinate c
+	 * 				Coordinate of a Jewel of which the western jewel is requested.
+	 * @return Coordinate
+	 * 				A Coordinate located west of Coordinate c.
 	 */
 	public Coordinate getWest() {
 		Coordinate WestJewel = new Coordinate((this.x - 1), this.y);
@@ -74,8 +83,10 @@ public class Coordinate {
 	/**
 	 * Getter method for acquiring a Jewel located south of a designated Jewel.
 	 * 
-	 * @param c Coordinate of a Jewel of which the southern jewel is requested.
-	 * @return A Coordinate located south of Coordinate c.
+	 * @param Coordinate c
+	 * 				Coordinate of a Jewel of which the southern jewel is requested.
+	 * @return Coordinate
+	 * 				A Coordinate located south of Coordinate c.
 	 */
 	public Coordinate getSouth() {
 		Coordinate SouthJewel = new Coordinate(this.x, (this.y - 1));
@@ -85,8 +96,10 @@ public class Coordinate {
 	/**
 	 * Getter method for acquiring a Jewel located east of a designated Jewel.
 	 * 
-	 * @param c Coordinate of a Jewel of which the eastern jewel is requested.
-	 * @return A Coordinate located east of Coordinate c.
+	 * @param Coordinate c 
+	 * 				Coordinate of a Jewel of which the eastern jewel is requested.
+	 * @return Coordinate
+	 * 				A Coordinate located east of Coordinate c.
 	 */
 	public Coordinate getEast() {
 		Coordinate EastJewel = new Coordinate((this.x + 1), this.y);
@@ -96,7 +109,8 @@ public class Coordinate {
 	/**
 	 * Method for checking if a Jewel object has a northern neighbour.
 	 * 
-	 * @return True if Jewel object has northern neighbour.
+	 * @return boolean
+	 * 				True if Jewel object has northern neighbour.
 	 */
 	public boolean hasNorth() {
 		return this.y <= 6;
@@ -105,7 +119,8 @@ public class Coordinate {
 	/**
 	 * Method for checking if a Jewel object has a western neighbour.
 	 * 
-	 * @return True if Jewel object has western neighbour.
+	 * @return boolean
+	 * 				True if Jewel object has western neighbour.
 	 */
 	public boolean hasWest() {
 		return this.x >= 1;
@@ -114,7 +129,8 @@ public class Coordinate {
 	/**
 	 * Method for checking if a Jewel object has a southern neighbour.
 	 * 
-	 * @return True if Jewel object has southern neighbour.
+	 * @return boolean
+	 * 				True if Jewel object has southern neighbour.
 	 */
 	public boolean hasSouth() {
 		return this.y >= 1;
@@ -123,16 +139,29 @@ public class Coordinate {
 	/**
 	 * Method for checking if a Jewel object has a eastern neighbour.
 	 * 
-	 * @return True if Jewel object has eastern neighbour.
+	 * @return boolean
+	 * 				True if Jewel object has eastern neighbour.
 	 */
 	public boolean hasEast() {
 		return this.x <= 6;
 	}
 	
+	/**
+	 * Method for replacing a Coordinate's current x-value for the designated x-value.
+	 * 
+	 * @param int x
+	 * 				Value of x.
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 	
+	/**
+	 * Method for replacing a Coordinate's current y-value for the designated y-value.
+	 * 
+	 * @param int y
+	 * 				Value of y.
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
@@ -140,7 +169,8 @@ public class Coordinate {
 	/**
 	 * Turns a Coordinate Object's information into a String.
 	 * 
-	 * @return String of designated Coordinate Object.
+	 * @return String
+	 * 				String of designated Coordinate Object.
 	 */
 	public String toString() {
 		return "Coordinate(" + x + "," + y + ")";
