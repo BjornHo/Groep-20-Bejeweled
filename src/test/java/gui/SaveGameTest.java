c21faf61c6e29efa06c0b99c1f8f61f936ee098f
Hugo Kooijman <H.V.Kooijman@gmail.com>
2015-09-25 15:33:54 +0200

    Created support classes for testing save games <testing

--- src/test/java/gui/SaveGameTest.java
+++ src/test/java/gui/SaveGameTest.java
@@ -2,28 +2,30 @@ package gui;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
-
 import board.Board;
 import game.Game;
-
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import xmlparser.XmlParser;
+
 
 public class SaveGameTest {
 	
-	@Mock private Board board;
-	@Mock private Game game;
+	@Mock private SupportGame game;
 	
 	@Before
 	public void setup () {
-
+		game = new SupportGame();
 	}
 	
 	@Test
 	public void SaveGameTest () {
+		XmlParser parser = new XmlParser();
+		parser.writeGame(game, "Testsavegame/Testsave");
+		
 		
 	}
 }
