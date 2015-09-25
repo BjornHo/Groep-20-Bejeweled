d4e7d231f43d26f0adeea217c43f639a3e6135b5
Hugo Kooijman <H.V.Kooijman@gmail.com>
2015-09-25 15:23:21 +0200

    SaveGameTest added <testing

---
+++ src/test/java/gui/SaveGameTest.java
@@ -0,0 +1,29 @@
+package gui;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+
+import board.Board;
+import game.Game;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+
+public class SaveGameTest {
+	
+	@Mock private Board board;
+	@Mock private Game game;
+	
+	@Before
+	public void setup () {
+
+	}
+	
+	@Test
+	public void SaveGameTest () {
+		
+	}
+}
