8b1338d7c06307d07040046901ae1351d861eb61
Rob Kapel <R.W.Kapel@student.tudelft.nl>
2015-10-08 21:58:59 +0200

    Added Composite Design Pattern + Double Match Functionality
    Match is now composed of MatchComponents, Matches and Coordinates are MatchComponents, still need to adjust the test cases <feature, organisation

--- src/main/java/board/Match.java
+++ src/main/java/board/Match.java
@@ -3,11 +3,11 @@ package board;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Match {
+public class Match extends MatchComponent {
 	/**
 	 * List of a Match Object containing the Coordinate Objects of matching jewels.
 	 */
-	private List<Coordinate> coordinates;
+	private List<MatchComponent> matchComponents;
 	
 	/**
 	 * -1 to show there is no max y-value yet.
@@ -15,7 +15,7 @@ public class Match {
 	private int ymax = -1;
 	
 	public Match() {
-		coordinates = new ArrayList<Coordinate>();
+		matchComponents = new ArrayList<MatchComponent>();
 	}
 	
 	/**
@@ -25,22 +25,26 @@ public class Match {
 	 *     Coordinate to be added.
 	 */
 	public void add(Coordinate coord) {
-		coordinates.add(coord);
+		matchComponents.add(coord);
 		if (ymax == -1 || coord.getY() > ymax) {
 			ymax = coord.getY();
 		}
 	}
 	
-	public Coordinate get(int coord) {
-		return coordinates.get(coord);
+	public MatchComponent get(int component) {
+		return matchComponents.get(component);
 	}
 	
-	public List<Coordinate> getCoordinates() {
-		return coordinates;
+	public void set(MatchComponent comp, int location) {
+		matchComponents.set(location, comp);
+	}
+	
+	public List<MatchComponent> getMatchComponents() {
+		return matchComponents;
 	}
 	
 	public int size() {
-		return coordinates.size();
+		return matchComponents.size();
 	}
 	
 	/**
@@ -50,16 +54,30 @@ public class Match {
 	 *     Amount of points.
 	 */
 	public int getPoints() {
+		int score = baseScore();
+		for (MatchComponent component : matchComponents) {
+			score += component.getPoints();
+		}
+		return score;
+	}
+	
+	public int baseScore() {
 		int size = size();
 		switch (size) {
 		case 3:
-			return 50;
+			return 200;
 		case 4:
-			return 100;
+			return 300;
 		case 5:
-			return 500;
+			return 750;
 		default:
 			return -1;
+		}
+	}
+	
+	public void clear(Board board) {
+		for (MatchComponent comp : matchComponents) {
+			comp.clear(board);
 		}
 	}
 	
@@ -69,17 +87,17 @@ public class Match {
 	 * @return boolean
 	 *     True if match is vertical.
 	 */
-	public boolean isVertical() {
-		int xcoord = -1;
-		for (Coordinate c : coordinates) {
-			if (xcoord == -1) {
-				xcoord = c.getX();
-			} else if (xcoord != c.getX()) {
-				return false;
-			}
-		}
-		return true;
-	}
+//	public boolean isVertical() {
+//		int xcoord = -1;
+//		for (MatchComponent m : matchComponents) {
+//			if (xcoord == -1) {
+//				xcoord = m.getX();
+//			} else if (xcoord != c.getX()) {
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
 	
 	/**
 	 * Checks if a match is horizontal.
@@ -87,20 +105,20 @@ public class Match {
 	 * @return boolean
 	 *     True if match is horizontal.
 	 */
-	public boolean isHorizontal() {
-		int ycoord = -1;
-		for (Coordinate c : coordinates) {
-			if (ycoord == -1) {
-				ycoord = c.getY();
-			} else if (ycoord != c.getY()) {
-				return false;
-			}
-		}
-		return true;
-	}
+//	public boolean isHorizontal() {
+//		int ycoord = -1;
+//		for (Coordinate c : matchComponents) {
+//			if (ycoord == -1) {
+//				ycoord = c.getY();
+//			} else if (ycoord != c.getY()) {
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
 	
 	/**
-	 * Returns minimum y-value of the Coordinates of a Match object.
+	 * Returns minimum y-value of the matchComponents of a Match object.
 	 * 
 	 * @return int
 	 *     Minimum y-value.
@@ -116,18 +134,28 @@ public class Match {
 		return ymax;
 	}
 	
+	public void setMatchValue(Board board, int location) {
+		for (MatchComponent comp : matchComponents) {
+			comp.setMatchValue(board, location);
+		}
+	}
+	
+	public int getMatchValue(Board board) {
+		return matchComponents.get(0).getMatchValue(board);
+	}
+	
 	/**
 	 * Returns x-value of a Match, if vertical.
 	 * 
 	 * @return int
 	 *     x-value.
 	 */
-	public int getX() {
-		if (isVertical()) {
-			return coordinates.get(0).getX();
-		}
-		return -1;
-	}
+//	public int getX() {
+//		if (isVertical()) {
+//			return matchComponents.get(0).getX();
+//		}
+//		return -1;
+//	}
 	
 	/**
 	 * Returns y-value of a Match, if Horizontal.
@@ -135,12 +163,12 @@ public class Match {
 	 * @return int
 	 *     y-value.
 	 */
-	public int getY() {
-		if (isHorizontal()) {
-			return coordinates.get(0).getY();
-		}
-		return -1;
-	}
+//	public int getY() {
+//		if (isHorizontal()) {
+//			return matchComponents.get(0).getY();
+//		}
+//		return -1;
+//	}
 	
 	@Override
 	public boolean equals(Object other) {
@@ -150,7 +178,7 @@ public class Match {
 				return false;
 			}
 			for (int i = 0; i < this.size(); i++) {
-				if (!this.coordinates.contains(that.coordinates.get(i))) {
+				if (!this.matchComponents.contains(that.matchComponents.get(i))) {
 					return false;
 				}
 			}
@@ -161,7 +189,7 @@ public class Match {
 	
 	@Override
 	public int hashCode() {
-		return ((coordinates.size() + 1) * ymax);
+		return ((matchComponents.size() + 1) * ymax);
 	}
 
 }
